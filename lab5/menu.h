2fdceed0044cec8fea803e25e35da5f91f4d0636
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2016-02-22 19:27:44 -0800

    added lab5

---
+++ lab5/menu.h
@@ -0,0 +1,57 @@
+#ifndef __MENU_CLASS__
+#define __MENU_CLASS__
+
+#include <iostream>
+#include "command.h"
+
+using namespace std;
+
+class Menu {
+	private:
+		int history_index;
+		vector<Command*> history;
+
+	public:
+		Menu() {
+			//Base constructor to set up all necessary members
+		};
+		void execute() {
+			if( history.size() != (unsigned)0 ){
+				cout << history.at(history_index)->execute() << endl;
+			}
+			else{
+				cout << "0" << endl;
+			//Prints the current commands value (based on history_index), if no commands exist
+			//print 0 by default
+			}
+		};
+		bool initialized() {
+			//Return true if the history has been primed with a single op instruction
+			//This is necessary because that is the base of a calculation
+			if(history.size() == (unsigned)1){
+				return true;
+			}
+			else{
+				return false;
+			}
+		};
+		void add_command(Command* cmd) {
+			//Adds a command to history in the appropriate posiiton (based on history_index)
+			vector<Command*>::iterator it = history.begin();
+			history.insert(it+history_index, cmd);
+		};
+		Command* get_command() {
+			//Returns the command in history we are currently referring to (based on history_index)
+			return history.at(history_index);
+		};
+		void undo() {
+			//Moves to the last command in history (if possible)
+			history_index -= 1;
+		};
+		void redo() {
+			//Moves to the next command in history (if possible)
+			history_index++;
+		};
+};
+
+#endif //__MENU_CLASS__
