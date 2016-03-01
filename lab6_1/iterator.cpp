4373f9427b802fbba49e670384ac4c1f3e83138d
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2016-02-29 17:32:08 -0800

    test

---
+++ lab6_1/iterator.cpp
@@ -0,0 +1,46 @@
+#include "iterator.h"
+
+//--------------------------------------------------------------------------
+// Unary Iterator Class
+//--------------------------------------------------------------------------
+UnaryIterator::UnaryIterator(){ self_ptr = ptr; }
+void UnaryIterator::first(){ current_ptr = self_ptr.get_left(); }
+void UnaryIterator::next(){ current_ptr = NULL; }
+bool UnaryIterator::is_done(){
+	if(current_ptr == NULL){ return true; }
+	else{ return false; }
+}
+Base* UnaryIterator::current(){ return current_ptr; }
+
+//--------------------------------------------------------------------------
+// Operator  Iterator Class
+//--------------------------------------------------------------------------
+OperatorIterator::OperatorIterator(){ self_ptr = ptr; }
+void OperatorIterator::first( current_ptr = self_ptr.get_left(); }
+void OperatorIterator::next(
+	if(current_ptr == ptr_left){ current_ptr = ptr_right; }
+	else{ current_ptr = NULL; }
+)
+bool OperatorIterator::is_done(a
+	if(current_ptr == NULL){ return true; }
+	else{ return false; }
+}
+Base* OperatorIterator::current(){ return current_ptr; }
+
+//--------------------------------------------------------------------------
+// PreOrder Iterator Class
+//--------------------------------------------------------------------------
+PreOrderIterator::PreOrderIterator(){ self_ptr = ptr; }
+void PreOrderIterator::first(){ 
+void PreOrderIterator::next(){}
+bool PreOrderIterator::is_done(){ return false; }
+Base* OperatorIterator::current(){ return current_ptr; }
+
+//--------------------------------------------------------------------------
+// Null Iterator Class
+//--------------------------------------------------------------------------
+NullIterator::NullIterator(){ self_ptr = ptr; }
+void NullIterator::first(){}
+void NullIterator::next(){}
+bool NullIterator::is_done(){ return true; }
+Base* OperatorIterator::current(){ return self_ptr; }
---
+++ lab6_1/iterator.h
@@ -0,0 +1,58 @@
+#ifndef __ITERATOR_CLASS__
+#define __ITERATOR_CLASS__
+#include "composite.h"
+
+class Base;
+
+class Iterator{
+	protected:
+		Base* self_ptr;
+		Base* current_ptr;
+
+	public:
+		Iterator (Base* ptr){ this-> self_ptr = ptr; }
+
+		// Sets up the iterator to start at the beginning of transversal
+		virtual void first() = 0;
+
+		// Move to the next element
+		virtual void next() = 0;
+
+		// Returns whether or not you've finished iterating
+		virtual bool is_done() = 0;
+
+		// Returns the element currently being pointed at
+		virtual Base* current() = 0;
+};
+
+class OperatorIterator : public Iterator{
+	public:
+		OperatorIterator (Base* ptr);
+
+		void first();
+		void next();
+		bool is_done();
+		Base* current();
+};
+
+class NullIterator : public Iterator{
+	public:
+		NullIterator (Base* ptr);
+
+		void first();
+		void next();
+		bool is_done(){}; 
+		Base* current(){};
+};
+
+class UnaryIterator : public Iterator{
+	public:
+		UnaryIterator (Base* ptr);
+
+		void first();
+		void next(){}; 
+		bool is_done();
+		Base* current();
+};
+
+#endif
