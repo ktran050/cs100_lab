d31b477db031f148a494806021249cfd1d0f0178
Junhao Liang <jlian020@hammer.cs.ucr.edu>
2016-02-29 18:12:14 -0800

    Updated iterator.cpp

--- lab6_1/iterator.cpp
+++ lab6_1/iterator.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------------------
 // Unary Iterator Class
 //--------------------------------------------------------------------------
-UnaryIterator::UnaryIterator( Base* ptr ){ self_ptr = ptr; }
+UnaryIterator::UnaryIterator( Base* ptr ) { this->self_ptr = ptr; }
 void UnaryIterator::first(){ current_ptr = self_ptr.get_left(); }
 void UnaryIterator::next(){ current_ptr = NULL; }
 bool UnaryIterator::is_done(){
@@ -15,13 +15,13 @@ Base* UnaryIterator::current(){ return current_ptr; }
 //--------------------------------------------------------------------------
 // Operator  Iterator Class
 //--------------------------------------------------------------------------
-OperatorIterator::OperatorIterator( Base* ptr ){ self_ptr = ptr; }
-void OperatorIterator::first( current_ptr = self_ptr.get_left(); }
+OperatorIterator::OperatorIterator( Base* ptr ) { this->self_ptr = ptr; }
+void OperatorIterator::first( current_ptr = self_ptr->get_left(); }
 void OperatorIterator::next(
 	if(current_ptr == ptr_left){ current_ptr = ptr_right; }
 	else{ current_ptr = NULL; }
 )
-bool OperatorIterator::is_done(a
+bool OperatorIterator::is_done()
 	if(current_ptr == NULL){ return true; }
 	else{ return false; }
 }
@@ -30,7 +30,7 @@ Base* OperatorIterator::current(){ return current_ptr; }
 //--------------------------------------------------------------------------
 // PreOrder Iterator Class
 //--------------------------------------------------------------------------
-PreOrderIterator::PreOrderIterator( Base* ptr ){ self_ptr = ptr; }
+PreOrderIterator::PreOrderIterator( Base* ptr ) { this->self_ptr = ptr; }
 void PreOrderIterator::first(){ 
 void PreOrderIterator::next(){}
 bool PreOrderIterator::is_done(){ return false; }
@@ -39,7 +39,7 @@ Base* OperatorIterator::current(){ return current_ptr; }
 //--------------------------------------------------------------------------
 // Null Iterator Class
 //--------------------------------------------------------------------------
-NullIterator::NullIterator( Base* ptr ){ self_ptr = ptr; }
+NullIterator::NullIterator( Base* ptr ) { this->self_ptr = ptr; }
 void NullIterator::first(){}
 void NullIterator::next(){}
 bool NullIterator::is_done(){ return true; }
